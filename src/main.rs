5b271d76f1d1c834d9a7a643ba4982f19f25fce4
Claus Guttesen <kometen@gmail.com>
2024-09-18 00:07:06 +0200

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,95 @@
+#[macro_use]
+extern crate rocket;
+
+use passwords::{analyzer, scorer, PasswordGenerator};
+use rocket::serde::json::Json;
+use serde::{Deserialize, Serialize};
+
+use std::default::Default;
+
+#[derive(Serialize, Deserialize, Default)]
+struct PasswordOptions {
+    count: Option<usize>,
+    length: Option<usize>,
+    numbers: Option<bool>,
+    lowercase_letters: Option<bool>,
+    uppercase_letters: Option<bool>,
+    symbols: Option<bool>,
+    spaces: Option<bool>,
+    exclude_similar_characters: Option<bool>,
+    strict: Option<bool>,
+}
+
+impl PasswordOptions {
+    fn new(count: usize, length: usize) -> PasswordOptions {
+        PasswordOptions {
+            count: Some(count),
+            length: Some(length),
+            ..Default::default()
+        }
+    }
+}
+
+#[derive(Serialize)]
+struct Pwd {
+    password: String,
+    score: u8,
+}
+
+#[get("/pwd")]
+fn get_pwd() -> Json<Vec<Pwd>> {
+    generate_passwords(&PasswordOptions::new(5, 20))
+}
+
+#[get("/pwd/<count>")]
+fn pwd_count(count: usize) -> Json<Vec<Pwd>> {
+    let c = count.min(31);
+    let password_options = PasswordOptions::new(c, 20);
+    generate_passwords(&password_options)
+}
+
+#[post("/pwd", data = "<password_options>")]
+fn post_pwd(password_options: Json<PasswordOptions>) -> Json<Vec<Pwd>> {
+    generate_passwords(&password_options)
+}
+
+fn generate_passwords(password_options: &PasswordOptions) -> Json<Vec<Pwd>> {
+    let c = password_options.count.unwrap_or(5);
+    let pwd_length = password_options.length.unwrap_or(20);
+    let option_numbers = password_options.numbers.unwrap_or(true);
+    let option_lowercase_letters = password_options.lowercase_letters.unwrap_or(true);
+    let option_uppercase_letters = password_options.uppercase_letters.unwrap_or(true);
+    let option_symbols = password_options.symbols.unwrap_or(false);
+    let option_spaces = password_options.spaces.unwrap_or(true);
+    let option_exclude_similar_characters =
+        password_options.exclude_similar_characters.unwrap_or(false);
+    let option_strict = password_options.strict.unwrap_or(false);
+
+    let pg = PasswordGenerator {
+        length: pwd_length,
+        numbers: option_numbers || option_strict,
+        lowercase_letters: option_lowercase_letters || option_strict,
+        uppercase_letters: option_uppercase_letters || option_strict,
+        symbols: option_symbols || option_strict,
+        spaces: option_spaces || option_strict,
+        exclude_similar_characters: option_exclude_similar_characters,
+        strict: option_strict,
+    };
+
+    let pwd: Vec<Pwd> = pg
+        .generate(c)
+        .unwrap()
+        .into_iter()
+        .map(|x| Pwd {
+            password: x.clone(),
+            score: scorer::score(&analyzer::analyze(&x)).ceil() as u8,
+        })
+        .collect();
+
+    Json(pwd)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![get_pwd, pwd_count, post_pwd])
+}
